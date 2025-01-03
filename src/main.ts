5cd0a0d60e1887c9608a9b32975ed56bd0cad217
zhuym <yiming.zhuym@dtzhejiang.com>
2025-01-03 15:12:56 +0800

    feat:页面修改

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 // 包装对象类型
 const str = new String("Hello, World!");
-console.log(typeof str); // object
-console.log(str[0]); // H
+// console.log(typeof str); // object
+// console.log(str[0]); // H
 
 // * Object 囊括了所有对象
 let obj: String | {};
@@ -24,6 +24,25 @@ let obj3 = 3;
 
 // 值类型
 const str2 = "Hello, World!";
-const x: 5 = 5;
-const y: number = 4 + 1;
-console.log(x === y); // true
+
+// 联合类型
+let x: "one" | "two" | "three" | "four" = "one";
+
+const printId = (id: number | string) => {
+  if (typeof id === "string") {
+    console.log(id.toUpperCase());
+    return;
+  }
+  console.log(id);
+};
+// printId(1);
+// printId("abc");
+const getPort = (schema: "http" | "https") => {
+  if (schema === "http") {
+    return 80;
+  }
+  return 443;
+};
+console.log(getPort("http"));
+
+// ================== 交叉类型 ==================
